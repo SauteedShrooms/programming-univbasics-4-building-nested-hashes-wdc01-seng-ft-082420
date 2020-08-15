bbcfc9d3909a0dd20040a40656ec979a3a8c68e0
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-15 21:24:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,18 +13,8 @@ def monopoly_with_second_tier
   {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owend => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
-      },
-      :names => {
-        :reading_railroad => {}
-        :pennsylvania_railroad => {}
-        :b_and_o_railroad => {}
-        :shortline_railroad => {}
-      }
+      :rent_in_dollars => {},
+      :names => {}
     }
   }
   
@@ -38,8 +28,18 @@ def monopoly_with_third_tier
   {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {},
-      :names => {}
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owend => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
+        :reading_railroad => {}
+        :pennsylvania_railroad => {}
+        :b_and_o_railroad => {}
+        :shortline_railroad => {}
+      }
     }
   }
   
